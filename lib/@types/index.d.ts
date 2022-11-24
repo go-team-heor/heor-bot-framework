8701f8dedb3e79d5efcbaf8af22a158027238ce7
Samantha Quiñones <samantha@ieatkillerbe.es>
2022-11-24 16:03:28 -0500

    text analyzer

--- lib/@types/index.d.ts
+++ lib/@types/index.d.ts
@@ -1,4 +1,5 @@
 export { analyzeText, detectLanguage } from "./text-analysis";
+export { Languages, translateText } from "./translate";
 export { MarkovGeneratorRepository, MarkovGenerator } from "./markov";
 export { Probability } from "./random";
 export { Plugin, PluginConfig, PluginMetadata, PluginPayload, PluginResult } from "./plugin";
